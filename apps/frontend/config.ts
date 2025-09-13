3bd3717f0ba5a71cfd2520b7aba5c60ecfc0953a
Rishik <86944435+RISHIK92@users.noreply.github.com>
2025-09-13 18:44:12 +0530

    Update config.ts

--- apps/frontend/config.ts
+++ apps/frontend/config.ts
@@ -1,3 +1,3 @@
 export const BACKEND_URL = "https://backend.nativex.rishik.codes";
-export const WORKER_URL = "https://worker.nativex.rishik.codes";
-export const WORKER_API_URL = "https://code.nativex.rishik.codes";
+export const WORKER_URL = "https://code.nativex.rishik.codes";
+export const WORKER_API_URL = "https://worker.nativex.rishik.codes";
