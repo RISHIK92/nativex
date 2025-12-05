ba3734004f4e50822e01e35db448178338274d2c
Rishik <rishik3555@gmail.com>
2025-12-06 00:37:06 +0530

    bug: backend url corrected

--- apps/frontend/config.ts
+++ apps/frontend/config.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "http://localhost:3002";
+export const BACKEND_URL = "https://backend.nativex.rishik.codes";
 export const WORKER_URL = "http://localhost:9090";
 export const WORKER_API_URL = "http://localhost:9090";
